87a1f332f25582088da2cbbc6df7e145d1901cf6
molodec <molodec@10.2.2.193>
2013-04-22 10:13:18 +0300

    With new CommonDAO

--- src/main/java/com/epam/lab/intouch/db/CommonDAO.java
+++ src/main/java/com/epam/lab/intouch/db/CommonDAO.java
@@ -1,7 +1,10 @@
 package com.epam.lab.intouch.db;
 
 import java.sql.SQLException;
+import java.util.Collection;
 import java.util.Map;
+
+import com.epam.lab.task.exception.DAOException;
 
 /**
  * Common DataAccessObject interface
@@ -12,8 +15,6 @@ import java.util.Map;
  */
 public interface CommonDAO<Type> {
 	
-	
-	
 	Map<Integer, Type> readAll() throws SQLException;
 
 	void create(Type bean) throws SQLException;
@@ -23,5 +24,15 @@ public interface CommonDAO<Type> {
 	void update(Type bean, int beanId) throws SQLException;
 	
 	void delete(int beanId) throws SQLException;
+	
+	/*void create(E entity) throws DAOException;
+
+	E getById(V id) throws DAOException;
+
+	void update(E oldEntity, E newEntity) throws DAOException;
+
+	void delete(E entity) throws DAOException;
+
+	Collection<E> getAll() throws DAOException;*/
 	
 }
