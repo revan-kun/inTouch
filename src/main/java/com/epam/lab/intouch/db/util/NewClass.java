f19d2a5c2b4c34395e681c0790edfd0a1d6e8d5d
zatorsky <zatorsky.danylo@gmail.com>
2013-04-22 13:36:55 +0300

    WOWWWWWWW Eclipse came back!!!

---
+++ src/main/java/com/epam/lab/intouch/db/util/NewClass.java
@@ -0,0 +1,13 @@
+package com.epam.lab.intouch.db.util;
+
+public class NewClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
