3e2a58f19bda922a0958e06cff33f494a5d7298b
molodec <molodec@10.2.2.193>
2013-04-23 11:37:06 +0300

    +Real have a skill

---
+++ src/main/java/com/epam/lab/intouch/dao/skill/SkillDAOImpl.java
@@ -0,0 +1,42 @@
+package com.epam.lab.intouch.dao.skill;
+
+import java.util.Collection;
+
+import com.epam.lab.intouch.dao.AbstractBaseDAO;
+import com.epam.lab.intouch.dao.exception.PersistenceException;
+import com.epam.lab.intouch.model.member.Member;
+
+public class SkillDAOImpl extends AbstractBaseDAO<Member, String> implements SkillDAO{
+
+	@Override
+	public void create(Member entity) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Member getById(String id) throws PersistenceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Member oldEntity, Member newEntity)
+			throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Member entity) throws PersistenceException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Collection<Member> getAll() throws PersistenceException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
