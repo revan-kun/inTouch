71f346b0753b6a641da50d9a69d3d511e9948e1e
Andriy Ovchar <ovchar.andr@gmail.com>
2013-04-29 14:59:13 +0300

    fix DB(delete technology, language and programming tables) and add field id into Skill.java

--- src/main/java/com/epam/lab/intouch/model/member/info/skill/Skill.java
+++ src/main/java/com/epam/lab/intouch/model/member/info/skill/Skill.java
@@ -1,11 +1,21 @@
 package com.epam.lab.intouch.model.member.info.skill;
 
 public class Skill {
-
+	
+	
+	private long id;
 	private String name;
 	private Double experience;
 	private String level;
 	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return name;
