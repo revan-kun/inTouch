fb5e1adeb43e0a4f6430cee28dabbcbff7e178cc
zatorsky <zatorsky.danylo@gmail.com>
2013-05-29 00:50:17 +0300

    +api for avatars fixed

--- src/main/java/com/epam/lab/intouch/web/servlet/api/rest/PhotoServiceServlet.java
+++ src/main/java/com/epam/lab/intouch/web/servlet/api/rest/PhotoServiceServlet.java
@@ -1,5 +1,8 @@
 package com.epam.lab.intouch.web.servlet.api.rest;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,35 +13,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.epam.lab.intouch.controller.exception.DataAccessingException;
 import com.epam.lab.intouch.controller.member.common.MemberController;
 import com.epam.lab.intouch.dao.exception.DAOException;
+import com.epam.lab.intouch.dao.project.DefaultProjectDAO;
+import com.epam.lab.intouch.dao.util.PropertiesReader;
 import com.epam.lab.intouch.model.member.Member;
 
 public class PhotoServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	private static final String PHOTO_PATH = "/img/user_avatar/";
+	private final static Logger LOG = LogManager.getLogger(DefaultProjectDAO.class);
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String memberLogin = request.getParameter("memberLogin");
+
+		String path = PropertiesReader.getProperty("avatar_storage");
 
 		MemberController memberController = new MemberController();
 		Member member = null;
 
 		try {
 			member = memberController.memberWithActiveProjectId(memberLogin);
+
 		} catch (DAOException e) {
-			// this block will be deleted
 			throw new IOException("Cannot access database!", e);
 		} catch (DataAccessingException e) {
 			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
 		}
 
 		if (member != null) {
-			try {
-				String photoName = member.getPhotoLink();
+			String photoLocation = path + "/" + member.getPhotoLink();
 
-				InputStream photo = getServletContext().getResourceAsStream(PHOTO_PATH + photoName);
+			try (InputStream photo = new BufferedInputStream(new FileInputStream(new File(photoLocation)));) {
+
+				LOG.debug("Photo location: " + photoLocation);
+
+				// InputStream photo = getServletContext().getResourceAsStream(PHOTO_PATH + photoName);
+				// InputStream photo = getServletContext().getResourceAsStream(photoLocation);
 
 				if (photo != null) {
 					response.setContentType("image/jpeg");
@@ -50,9 +64,7 @@ public class PhotoServiceServlet extends HttpServlet {
 					while ((read = photo.read(data)) != -1) {
 						out.write(data, 0, read);
 					}
-				}else{
-					response.sendError(HttpServletResponse.SC_NOT_FOUND);
-				}
+				} 
 			} catch (FileNotFoundException e) {
 				response.sendError(HttpServletResponse.SC_NOT_FOUND);
 			}
